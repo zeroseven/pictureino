b8826ac6252fda242fa07fe4c186bc05b78f0ca8
Raphael Thanner <r.thanner@zeroseven.de>
2025-05-21 22:33:41 +0200

    Remove cache DI and replace by TYPO3 CachingFrameworkStorage

--- Classes/Utility/RateLimiterUtility.php
+++ Classes/Utility/RateLimiterUtility.php
@@ -5,34 +5,31 @@ declare(strict_types=1);
 namespace Zeroseven\Pictureino\Utility;
 
 use Symfony\Component\RateLimiter\RateLimiterFactory;
-use Symfony\Component\RateLimiter\Storage\CacheStorage;
-use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
+use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\RateLimiter\Storage\CachingFrameworkStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RateLimiterUtility
 {
-    private const IP_REQUEST_LIMIT = 4;
+    private const IP_REQUEST_LIMIT = 100;
     private const IP_REQUEST_TIME = '1 hour';
-    private const IMAGE_REQUEST_LIMIT = 2;
+    private const IMAGE_REQUEST_LIMIT = 20;
     private const IMAGE_REQUEST_TIME = '1 hour';
 
     private string $identifier;
-    private FrontendInterface $cache;
 
     public function __construct(string $identifier)
     {
         $this->identifier = $identifier;
     }
 
-    public function injectCache(FrontendInterface $cache): void
+    protected function getStorage(): CachingFrameworkStorage
     {
-        $this->cache = $cache;
-    }
-
-    protected function getStorage(): CacheStorage
-    {
-        return GeneralUtility::makeInstance(CacheStorage::class, $this->cache);
+        return GeneralUtility::makeInstance(
+            CachingFrameworkStorage::class,
+            GeneralUtility::makeInstance(CacheManager::class)
+        );
     }
 
     protected function loggedIn(): bool
@@ -50,7 +47,7 @@ class RateLimiterUtility
             'rate' => ['interval' => self::IP_REQUEST_TIME],
         ], $this->getStorage());
 
-        return $rateLimit->create(md5($ip))->consume()->isAccepted();
+        return !$rateLimit->create(md5($ip))->consume()->isAccepted();
     }
 
     protected function imageLimitExceeded(): bool
@@ -61,17 +58,16 @@ class RateLimiterUtility
             'limit' => self::IMAGE_REQUEST_LIMIT,
             'rate' => ['interval' => self::IMAGE_REQUEST_TIME],
         ], $this->getStorage());
-        die('lol');
-        die('"3');
 
-        return $rateLimit->create($this->identifier)->consume()->isAccepted();
+        return !$rateLimit->create($this->identifier)->consume()->isAccepted();
     }
 
     public function limitExceeded(): bool
     {
-        return $this->ipLimitExceeded();
-       return !$this->loggedIn()
-        && !$this->ipLimitExceeded()
-        && !$this->imageLimitExceeded();
+        if ($this->loggedIn()) {
+            return false;
+        }
+
+        return $this->ipLimitExceeded() || $this->imageLimitExceeded();
     }
 }
